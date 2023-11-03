1abad6661ffd845f6cbb4fb6c954efc252431f20
Kyohei Uto <im@kyoheiu.dev>
2023-11-04 05:40:15 +0900

    Add svelte, tailwind

---
+++ src/components/Top.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Links from "./Link.svelte";
+</script>
+
+<div class="mt-4 flex flex-col items-center">
+	<div class="w-20 h-20">
+		<img src="avatar.png" class="rounded-full" alt="my avatar" />
+	</div>
+
+	<!-- Title -->
+	<h1 class="text-2xl m-2 mt-4">Kyohei Uto</h1>
+
+	<!-- Short Bio -->
+	<div class="text-center m-2 mb-6 w-48">
+		Husband, father, editor, working student@UoL, and developer in the wild.
+	</div>
+
+	<Links />
+
+	<div class="text-center mt-6 mb-2 w-48">Powered by Astro, illustration by my wife.</div>
+</div>
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,11 +4,11 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		date: z.date(),
+		math: z.boolean(),
+		categories: z.array(z.string()),
+		tags:z.array(z.string()),
 	}),
 });
 
